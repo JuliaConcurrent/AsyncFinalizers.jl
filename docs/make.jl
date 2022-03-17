8ce2671994401ddd09bae760c3169d652618fc2c
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-17 06:01:44 -0400

    Setup documentation

---
+++ docs/make.jl
@@ -0,0 +1,33 @@
+using Documenter
+using AsyncFinalizers
+
+makedocs(
+    sitename = "AsyncFinalizers",
+    format = Documenter.HTML(),
+    modules = [AsyncFinalizers],
+    strict = [
+        :autodocs_block,
+        :cross_references,
+        :docs_block,
+        :doctest,
+        :eval_block,
+        :example_block,
+        :footnote,
+        :linkcheck,
+        :meta_block,
+        # :missing_docs,
+        :parse_error,
+        :setup_block,
+    ],
+    # Ref:
+    # https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.makedocs
+
+)
+
+deploydocs(
+    repo = "github.com/JuliaConcurrent/AsyncFinalizers.jl",
+    devbranch = "main",
+    push_preview = true,
+    # Ref:
+    # https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.deploydocs
+)
